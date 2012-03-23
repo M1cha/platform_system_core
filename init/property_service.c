f1ab0dd3a3d1a4aa3497151a1033fa6cbe4a9223
Annamalai Lakshmanan <annamalai.l@samsung.com>
2012-03-24 02:26:37 +0530

    origen: Granting permissions for StageFright
    Allowing stagefright process(MEDIA) to modify property
    variables 'dev.', this will allow sharing data and IPC with
    SurfaceFlinger.
    
    Change-Id: Iede2559952bbb87dfb9a4283abe7974516e5618b
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- init/property_service.c
+++ init/property_service.c
@@ -85,6 +85,8 @@ struct {
     { "persist.sys.",     AID_SYSTEM,   0 },
     { "persist.service.", AID_SYSTEM,   0 },
     { "persist.security.", AID_SYSTEM,   0 },
+    /* Adding for Origen HDMI support */
+    { "dev.",             AID_MEDIA,    0 },
     { NULL, 0, 0 }
 };
 
